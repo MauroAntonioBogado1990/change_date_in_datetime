d23c387084f1d0a3b5e0f8df2a1be70babaa8e8a
MauroAntonioBogado1990 <mauroantoniobogado@gmail.com>
2023-11-07 14:48:33 -0300

    add change field datetime to date

---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding:utf-8 -*-
+
+{
+    'name': 'Change Date In DateTime',
+    'version': '16.0',
+    'depends': [
+        'base','sale',
+    ],
+    'author': 'Mauro Bogado, Exemax',
+    'website': '"http://www.exemax.com.ar"',
+    'summary': 'Modulo cambia tipo de dato de datetime a date',
+    'category': 'Extra Tools',
+    'description': '''
+   Modulo cambia tipo de dato de datetime a date.
+    ''',
+    'data': [
+        'views/change_date_in_datetime.xml',
+        
+       
+    ],
+}
